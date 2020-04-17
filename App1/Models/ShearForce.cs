a1bfc7be0ad512d1b13691b7da030848d467dde9
Samuel <sjbc35@mail.missouri.edu>
2020-04-17 13:08:36 -0500

    Added a .gitignore for debug and release build pushes. Additionally added a view model and BendingMoment and ShearMoment classes to attach to the SfChart. Updated Axises.

---
+++ App1/Models/ShearForce.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalForcesCalculator.Models
+{
+    public class ShearForce
+    {
+        public float XCoord { get; set; }
+        public float YCoord { get; set; }
+    }
+}
---
+++ App1/ViewModel/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using InternalForcesCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalForcesCalculator.ViewModel
+{
+    public class ViewModel
+    {
+        public List<ShearForce> ShearForceData { get; set; }
+        public List<BendingMoment> BendingMomentData { get; set; }
+
+        public ViewModel()
+        {
+            ShearForceData = new List<ShearForce>()
+            {
+                new ShearForce { XCoord = 10, YCoord = 10 },
+                new ShearForce { XCoord = 20, YCoord = 20 },
+                new ShearForce { XCoord = 30, YCoord = 30 },
+                new ShearForce { XCoord = 40, YCoord = 40 }
+            };
+
+            BendingMomentData = new List<BendingMoment>()
+            {
+                new BendingMoment { XCoord = 10, YCoord = 10 },
+                new BendingMoment { XCoord = 20, YCoord = 20 },
+                new BendingMoment { XCoord = 30, YCoord = 30 },
+                new BendingMoment { XCoord = 40, YCoord = 40 }
+            };
+        }
+    }
+}
