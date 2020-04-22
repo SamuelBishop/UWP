dd9f08e2e2b59e9ad024bcd26954fdb0926f3bd1
Samuel <sjbc35@mail.missouri.edu>
2020-04-21 20:14:43 -0500

    Attempting to get Coordinate Pair generation to work. Something is wrong with my XAML-C# binding but I'm not sure what. Posted on syncfusion form for help.

--- App1/ViewModel/ViewModel.cs
+++ App1/ViewModel/ViewModel.cs
@@ -13,24 +13,85 @@ namespace InternalForcesCalculator.ViewModel
     {
         public List<ShearForce> ShearForceData { get; set; }
         public List<BendingMoment> BendingMomentData { get; set; }
+        
+        public float PointLoadingLocation { get; set; }
+        public float PointLoadingMagnitude { get; set; }
+        public float TriangularDistributedLoadingLocation { get; set; }
+        public float TriangularDistributedLoadingMagnitude { get; set; }
+        public float RectangularDistributedLoadingLocation { get; set; }
+        public float RectangularDistributedLoadingMagnitude { get; set; }
+        public float FreeMomentLocation { get; set; }
+        public float FreeMomentMagnitude { get; set; }
+        public float PinSupportLocation { get; set; }
+        public float RollerSupportLocation { get; set; }
+        public float FixedSupportLocation { get; set; }
 
         public ViewModel()
         {
-            ShearForceData = CreateShearForceModel();
-            BendingMomentData = CreateBendingMomentModel();
+            ShearForceData = CreateShearForceModel(
+                PointLoadingLocation, 
+                PointLoadingMagnitude, 
+                TriangularDistributedLoadingLocation, 
+                TriangularDistributedLoadingMagnitude, 
+                RectangularDistributedLoadingLocation, 
+                RectangularDistributedLoadingMagnitude, 
+                FreeMomentLocation, 
+                FreeMomentMagnitude,
+                PinSupportLocation,
+                RollerSupportLocation,
+                FixedSupportLocation
+                );
+            BendingMomentData = CreateBendingMomentModel(
+                PointLoadingLocation,
+                PointLoadingMagnitude,
+                TriangularDistributedLoadingLocation,
+                TriangularDistributedLoadingMagnitude,
+                RectangularDistributedLoadingLocation,
+                RectangularDistributedLoadingMagnitude,
+                FreeMomentLocation,
+                FreeMomentMagnitude,
+                PinSupportLocation,
+                RollerSupportLocation,
+                FixedSupportLocation
+                );
         }
 
-        public List<ShearForce> CreateShearForceModel()
+        public List<ShearForce> CreateShearForceModel(
+            float PointLoadingLocation,
+            float PointLoadingMagnitude,
+            float TriangularDistributedLoadingLocation,
+            float TriangularDistributedLoadingMagnitude,
+            float RectangularDistributedLoadingLocation,
+            float RectangularDistributedLoadingMagnitude,
+            float FreeMomentLocation,
+            float FreeMomentMagnitude,
+            float PinSupportLocation,
+            float RollerSupportLocation,
+            float FixedSupportLocation
+            )
         {
             List<ShearForce> Result = new List<ShearForce>()
             {
-                new ShearForce { XCoord = 0, YCoord = 0 },
+                new ShearForce { XCoord = PointLoadingLocation, YCoord = PointLoadingMagnitude },
+                new ShearForce { XCoord = TriangularDistributedLoadingLocation, YCoord = TriangularDistributedLoadingMagnitude }
             };
 
             return Result;
         }
 
-        public List<BendingMoment> CreateBendingMomentModel()
+        public List<BendingMoment> CreateBendingMomentModel(
+            float PointLoadingLocation,
+            float PointLoadingMagnitude,
+            float TriangularDistributedLoadingLocation,
+            float TriangularDistributedLoadingMagnitude,
+            float RectangularDistributedLoadingLocation,
+            float RectangularDistributedLoadingMagnitude,
+            float FreeMomentLocation,
+            float FreeMomentMagnitude,
+            float PinSupportLocation,
+            float RollerSupportLocation,
+            float FixedSupportLocation
+            )
         {
             List<BendingMoment> Result = new List<BendingMoment>()
             {
